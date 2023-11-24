4cee3cc8121dec6832f57459c3f0843c84b3e31e
Deepak <deepakpunyani@ymail.com>
2023-11-24 17:10:58 +0530

    express and routes

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 const PORT = 8080;
-//const mFun =  require('./MathsFun');
-const {minus,sums} =  require('./MathsFun');
-const url = require('url'); 
+const users = [
+    [1,'Sada','Oloshkin','soloshkin0@geocities.jp','Female'],
+    [2,'Vance','Showalter','vshowalter1@nature.com','Male'],
+    [3,'Adan','McLice','amclice2@whitehouse.gov','Female'],
+    [4,'Benjy','Lohoar','blohoar3@issuu.com','Male'],
+    [5,'Ario','Pynner','apynner4@moonfruit.com','Male'],
+    [6,'Alva','Trevan','atrevan5@lycos.com','Genderfluid'],
+]
 
-const FinalServer = http.createServer((req, res) => {
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log("Server Run",req.url);
-    //console.log(`${mFun}`);
-    //console.log(mFun(5,8));
-    //console.log(mFun.sums(5,8));
-    console.log(minus(5,8));
-    console.log(url.parse(req.url))
-
-    if(req.url == "/" || req.url == "/home"){
-        res.write('Home Page');
-
-    }else if( req.url == "/about"){
-        res.write('About Page');
-
-    }else{
-        res.write('404 Page');
-    }
-    res.end("Server End");
+app.get("/",(req,res) =>{
+    return res.send("Hocme");
 });
 
-FinalServer.listen(PORT,(req, res) => {
+app.get("/about",(req,res) =>{
+    return res.send("about pageh");
+});
+
+
+app.get("/apis/users",(req,res) =>{
+    return res.json(users);
+});
+
+app.get("/apis/users/:id",(req,res) =>{
+    return res.json({"name":"test","id":req.params.id});
+});
+ 
+app.delete("/apis/users/:id",(req,res) =>{
+    return res.json({"name":"test","id":req.params.id});
+});
+ 
+app.patch("/apis/users/:id",(req,res) =>{
+    return res.json({"name":"test","id":req.params.id});
+});
+
+app.post("/apis/users/:id",(req,res) =>{
+    return res.json({"name":"test","id":req.params.id});
+});
+ 
+
+// const FinalServer = http.createServer(app);
+
+// FinalServer.listen(PORT,(req, res) => {
+//     console.log(`http://localhost:${PORT}/`);
+// });
+
+app.listen(PORT,(req, res) => {
     console.log(`http://localhost:${PORT}/`);
 });
