a6cc1db9fe07087dd0b95ef7d4964fad80c5b804
Deepak <deepakpunyani@ymail.com>
2023-12-22 17:04:19 +0530

    controller and routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,26 @@
-async function users(){
+async function users(req , res){
     return res.status(265).json({"name":"test"});
+}
+
+async function singleuser(req , res){
+    return res.json({"name":"test","idf":req.params.id});
+
+}
+
+async function updateuser(req , res){
+    return res.json({"name":"updateuser","idf":req.params.id});
+
+}
+
+async function deleteuser(req , res){
+    return res.json({"name":"delete","idf":req.params.id});
+
+}
+
+
+module.exports  = {
+    users,
+    singleuser,
+    updateuser,
+    deleteuser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { users,singleuser , updateuser , deleteuser } = require('../controllers/user');
 
 
-router.get("/", async (req,res) =>{
-    console.log(req.body);
-    return res.status(265).json({"name":"test"});
-});
+router.get("/", users);
 
-router.route("/:id").get(async (req,res) =>{
-    return res.json({"name":"test","id":req.params.id});
-}).delete(async (req,res) =>{
-    return res.json({"name":"tdest","id":req.params.id});
-}).patch(async (req,res) =>{
-    return res.json({"name":"tpest","id":req.params.id});
-}).post(async (req,res) =>{
+router.route("/:id").get(singleuser).delete(deleteuser).patch(updateuser).post(async (req,res) =>{
     return res.json({"name":"teppst","id":req.params.id});
 });
  
