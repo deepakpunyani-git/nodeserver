ebbb9054bbe3af42197eb94a9a22c9d098c2bf71
Deepak <deepakpunyani@ymail.com>
2024-01-03 15:23:42 +0530

    Upload iamge

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix+'.png')
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -11,6 +23,16 @@ app.set('views',path.resolve("./views"));
 const {connectDB} = require('./connection.js');
 connectDB("mongodb://0.0.0.0:27017/myprojects").then((err) =>{
     console.log("mongoDB connected")
+})
+
+
+app.post('/uploads', upload.single('avatar'), function (req, res, next) {
+    console.log(req.file, req.body)
+    console.log(req.file)
+
+    next();
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
 })
 
 
